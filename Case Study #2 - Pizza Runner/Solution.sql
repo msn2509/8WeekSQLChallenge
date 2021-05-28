f5558f9f006d021ea70eb4da7b86c89869a340f2
Madhura Narkar <55361197+msn2509@users.noreply.github.com>
2021-05-28 07:14:54 -0500

    Create Solution.sql

--- Case Study #2 - Pizza Runner/Solution.sql
+++ Case Study #2 - Pizza Runner/Solution.sql
@@ -1,1 +1,27 @@
 
+--Pizza Metrics
+--How many pizzas were ordered?
+select 
+  count(pizza_id) as no_pizzas
+from customer_orders
+
+--Output
+--| no_pizzas |
+--| --------- |
+--| 14        |
+
+
+--How many unique customer orders were made?
+select 
+	count(distinct order_id) as unique_cust_orders
+from customer_orders ;
+
+--Output
+--| unique_cust_orders |
+--| ------------------ |
+--| 10                 |
+
+
+
+
+
