6bd6f4be7cca70a973ce9597597c794407f5ca89
Madhura Narkar <55361197+msn2509@users.noreply.github.com>
2021-05-28 08:07:19 -0500

    Update with code for #5,6

--- Case Study #2 - Pizza Runner/Solution.sql
+++ Case Study #2 - Pizza Runner/Solution.sql
@@ -55,3 +55,49 @@ order by pizza_id
 --| 1        | 9      |
 --| 2        | 3      |
 
+
+--How many Vegetarian and Meatlovers were ordered by each customer?
+select
+	customer_id
+	, pizza_name
+	, count(*) as amount
+from runner_orders run_ord 
+left join customer_orders cust_ord on run_ord.order_id = cust_ord.order_id
+left join pizza_names names on cust_ord.pizza_id = names.pizza_id
+group by pizza_name, customer_id
+order by customer_id, pizza_name
+
+--Output
+
+--| customer_id | pizza_name | amount |
+--| ----------- | ---------- | ------ |
+--| 101         | Meatlovers | 2      |
+--| 101         | Vegetarian | 1      |
+--| 102         | Meatlovers | 2      |
+--| 102         | Vegetarian | 1      |
+--| 103         | Meatlovers | 3      |
+--| 103         | Vegetarian | 1      |
+--| 104         | Meatlovers | 3      |
+--| 105         | Vegetarian | 1      |
+
+
+
+-- What was the maximum number of pizzas delivered in a single order?
+
+select
+	count(pizza_id) as no_pizza
+from runner_orders run_ord
+left join customer_orders cust_ord on run_ord.order_id = cust_ord.order_id
+where lower(cancellation) not like '%cancellation%' or cancellation is null
+group by run_ord.order_id
+order by count(pizza_id) desc
+limit 1
+
+--Output 
+
+--| no_pizza |
+--| -------- |
+--| 3        |
+
+
+-- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
