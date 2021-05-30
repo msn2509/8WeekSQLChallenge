f987aa1efa313d9bb4096711e292aebe88676a2d
Madhura Narkar <55361197+msn2509@users.noreply.github.com>
2021-05-30 09:15:06 -0500

    Update Ingredient Optimisation Solutions.sql

--- Case Study #2 - Pizza Runner/Ingredient Optimisation Solutions.sql
+++ Case Study #2 - Pizza Runner/Ingredient Optimisation Solutions.sql
@@ -31,3 +31,70 @@ order by pizza_id
 --| 2        | Onions       |
 --| 2        | Peppers      |
 --| 2        | Tomatoes     |
+
+
+-- What was the most commonly added extra?
+
+with extra_cte as (
+  select 
+    unnest(string_to_array(extras, ',') ) as extras
+    ,count(*) as count_extras
+  from customer_orders
+  where extras is not null and  extras!='null' and extras!=''
+  group by unnest(string_to_array(extras, ',') )
+)
+ 
+select 
+  topping_name as most_common_extra 
+from (
+  select *
+    ,rank() over (order by count_extras desc) as order_extra
+  from extra_cte cte 
+  left join pizza_toppings t on cast(cte.extras as int) = t.topping_id
+)a 
+where order_extra=1
+
+--Output
+
+--| most_common_extra |
+--| ----------------- |
+--| Bacon             |
+
+
+-- What was the most common exclusion?
+
+with exclusions_cte as (
+  select 
+    unnest(string_to_array(exclusions, ',') ) as exclusions
+    ,count(*) as count_exclusions
+  from customer_orders
+  where exclusions is not null and  exclusions!='null' and exclusions!=''
+  group by unnest(string_to_array(exclusions, ',') )
+  )
+ 
+ select 
+    topping_name as most_common_exclusions 
+ from (
+   select *
+    , rank() over (order by count_exclusions desc) as order_exclusions
+   from exclusions_cte cte 
+   left join pizza_toppings t on cast(cte.exclusions as int) = t.topping_id
+   )a 
+where order_exclusions=1
+
+-- Output
+
+--| most_common_exclusions |
+--| ---------------------- |
+--| Cheese                 |
+
+
+
+
+
+
+
+
+
+
+
