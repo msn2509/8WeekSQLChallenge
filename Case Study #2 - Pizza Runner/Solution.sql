62666677c1a31b3149892414ce7b8cb0adaf8e70
Madhura Narkar <55361197+msn2509@users.noreply.github.com>
2021-05-29 20:57:34 -0500

    Completing all pizza metrics

--- Case Study #2 - Pizza Runner/Solution.sql
+++ Case Study #2 - Pizza Runner/Solution.sql
@@ -100,4 +100,166 @@ limit 1
 --| 3        |
 
 
--- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+--For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+select 
+	customer_id
+	, sum(change_flag) as pizza_with_changes
+	, sum(case when change_flag=0 then 1 else 0 end ) as pizza_with_no_changes
+from(
+	select 
+		customer_id, 
+		pizza_id, 
+		case when (exclusions not in ('', 'null') and exclusions is not NULL) or ( extras not in ('', 'null') and extras is not NULL) then 1 else 0 end as change_flag
+	from customer_orders 
+  )a
+group by customer_id
+order by customer_id
+
+--Output
+
+--| customer_id | pizza_with_changes | pizza_with_no_changes |
+--| ----------- | ------------------ | --------------------- |
+--| 101         | 0                  | 3                     |
+--| 102         | 0                  | 3                     |
+--| 103         | 4                  | 0                     |
+--| 104         | 2                  | 1                     |
+--| 105         | 1                  | 0                     |
+
+
+--How many pizzas were delivered that had both exclusions and extras?
+
+select 
+	customer_id
+	,sum(change_flag) as pizza_with_changes
+from(
+	select 
+		customer_id, 
+		pizza_id, 
+		case when (exclusions not in ('', 'null') and exclusions is not NULL) and ( extras not in ('', 'null') and extras is not NULL) then 1 else 0 end as change_flag
+	from customer_orders 
+  )a
+  group by customer_id
+  order by customer_id
+  
+--Output
+--| customer_id | pizza_with_changes |
+--| ----------- | ------------------ |
+--| 101         | 0                  |
+--| 102         | 0                  |
+--| 103         | 1                  |
+--| 104         | 1                  |
+--| 105         | 0                  |
+
+
+--What was the total volume of pizzas ordered for each hour of the day?
+
+with hour_cte as 
+(select 1 as hour 
+ union 
+ select 2 as hour
+  union 
+ select 3 as hour
+  union 
+ select 4 as hour
+  union 
+ select 5 as hour
+  union 
+ select 6 as hour
+  union 
+ select 7 as hour
+  union 
+ select 8 as hour
+  union 
+ select 9 as hour
+  union 
+ select 10 as hour
+  union 
+ select 11 as hour
+  union 
+ select 12 as hour
+  union 
+ select 13 as hour
+  union 
+ select 14 as hour
+  union 
+ select 15 as hour
+  union 
+ select 16 as hour
+  union 
+ select 17 as hour
+  union 
+ select 18 as hour union 
+ select 19 as hour union 
+ select 20 as hour
+  union 
+ select 21 as hour union 
+ select 22 as hour union 
+ select 23 as hour union 
+ select 00 as hour
+ )
+
+
+select h.hour as hour, count(order_id)
+ from hour_cte h
+left join customer_orders cus on h.hour = date_part('hour',order_time)
+group by h.hour
+order by h.hour
+
+--Output
+--| hour | count |
+--| ---- | ----- |
+--| 0    | 0     |
+--| 1    | 0     |
+--| 2    | 0     |
+--| 3    | 0     |
+--| 4    | 0     |
+--| 5    | 0     |
+--| 6    | 0     |
+--| 7    | 0     |
+--| 8    | 0     |
+--| 9    | 0     |
+--| 10   | 0     |
+--| 11   | 1     |
+--| 12   | 0     |
+--| 13   | 3     |
+--| 14   | 0     |
+--| 15   | 0     |
+--| 16   | 0     |
+--| 17   | 0     |
+--| 18   | 3     |
+--| 19   | 1     |
+--| 20   | 0     |
+--| 21   | 3     |
+--| 22   | 0     |
+--| 23   | 3     |
+
+
+-- What was the volume of orders for each day of the week?
+-- The day of the week starts from a monday as per the function below. 
+
+with dow_cte as 
+(select 1 as dow 
+ union 
+ select 2 as dow
+  union 
+ select 3 as dow
+  union 
+ select 4 as dow
+  union 
+ select 5 as dow
+  union 
+ select 6 as dow
+  union 
+ select 0 as dow)
+
+select dow, count(order_id) as total_orders
+from dow_cte d 
+left join customer_orders cus on d.dow=extract('dow' from order_time)
+group by dow
+order by dow
+
+
+
+
