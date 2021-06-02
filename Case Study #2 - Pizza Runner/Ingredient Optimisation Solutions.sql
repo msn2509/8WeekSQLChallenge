af70f738a4e9df406bb125923d8b90e30b5a2e94
Madhura Narkar <55361197+msn2509@users.noreply.github.com>
2021-06-01 21:24:31 -0500

    Update Ingredient Optimisation Solutions.sql

--- Case Study #2 - Pizza Runner/Ingredient Optimisation Solutions.sql
+++ Case Study #2 - Pizza Runner/Ingredient Optimisation Solutions.sql
@@ -157,6 +157,97 @@ order by row_nbr;
 
 
 
+-- Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+--  For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
+
+with pk_cte as (
+  SELECT
+    row_number() over () as row_nbr
+    , * 
+  from customer_orders)
+
+, topping_total as (
+  select 
+    row_nbr
+    , pk.pizza_id
+    ,unnest(string_to_array(toppings,',') ) as toppings
+  from pk_cte pk
+  left join pizza_recipes r on pk.pizza_id = r.pizza_id
+
+  union all
+
+  select 
+    row_nbr
+    , pizza_id
+    ,unnest(case 
+              when extras is not null and  extras!='null' and extras!='' then string_to_array(extras,',') 
+              else (array[null])::text[]  end) as extras
+  from pk_cte 
+
+  union all 
+  select 
+    row_nbr
+    , pizza_id
+    , unnest(case 
+             when exclusions is not null and  exclusions!='null' and exclusions!='' then string_to_array(exclusions,',') 
+             else (array[null])::text[]  end) as exclusions
+  from pk_cte
+)
+, desc_finder as (
+  select 
+    row_nbr
+    , pizza_id
+    , toppings
+    , topping_name
+    , count(*) as x_factor 
+  from topping_total t
+  left join pizza_toppings top on cast(t.toppings as int) = top.topping_id           
+  group by row_nbr, toppings, topping_name, pizza_id
+),
+            
+final_data as (            
+  select 
+    row_nbr
+    , pizza_name
+    ,toppings
+    , case 
+      when cast(string_agg(x_factor::text, '') as int) > 1 and toppings is not null then concat(string_agg(x_factor::text, ''),'x',string_agg(topping_name::text, ',')) 
+      else string_agg(topping_name::text, '') end as final_toppings
+  from desc_finder des
+  left join  pizza_names pnames on des.pizza_id = pnames.pizza_id                                           
+  group by row_nbr, pizza_name, toppings        
+ )
+
+select 
+  concat(pizza_name,' : ',string_agg(final_toppings::text, ','))  as ingredients_list                                       
+from final_data 
+group by row_nbr, pizza_name
+
+
+--Output
+
+--| ingredients_list                                                                            |
+--| ------------------------------------------------------------------------------------------- |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon                 |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon                 |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon                 |
+--| Vegetarian : Tomatoes,Tomato Sauce,Mushrooms,Onions,Peppers,Cheese                          |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon,Cheese          |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon,Cheese          |
+--| Vegetarian : Tomatoes,Tomato Sauce,Mushrooms,Onions,Peppers,2xCheese                        |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,2xBacon               |
+--| Vegetarian : Tomatoes,Tomato Sauce,Mushrooms,Onions,Peppers,Cheese                          |
+--| Vegetarian : Tomatoes,Tomato Sauce,Mushrooms,Onions,Peppers,Bacon,Cheese                    |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon                 |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,2xChicken,Mushrooms,Pepperoni,2xBacon,Cheese      |
+--| Meatlovers : Salami,BBQ Sauce,Beef,Cheese,Chicken,Mushrooms,Pepperoni,Bacon                 |
+--| Meatlovers : Salami,BBQ Sauce,Beef,2xCheese,Chicken,2xMushrooms,Pepperoni,2xBacon,BBQ Sauce |
+
+
+            
+            
+
+
 
 
 
